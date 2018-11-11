61891a261e442ccd36e81d0d6c06f82d7b17ce54
maahi28283 <44293861+maahi28283@users.noreply.github.com>
2018-11-11 17:32:55 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+       int n=20,num=1;
+       for(int i=1;i<=n;i++){
+        num=num*i;
+       }
+        System.out.println("num " + num);
+     }
+}
